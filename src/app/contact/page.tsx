e45c6868ce5f640c3673e27429d2ebdf7db71a9e
Prem <prmk1406@gmail.com>
2025-02-20 15:19:57 +0530

    Fixed contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import {ContactUs} from "@/sections/ContactUs";
 import { Contact } from "@/sections/Contact";
 
 export default function ContactPage() {
   return( 
     <div className="min-h-screen py-16">
 
-  <Contact/>
-  </div>
+        <Contact/>
+    
+     </div>
   );
 }
