db7f81de799fb65059a9668e8c964bf5e87d8f77
Prem <prmk1406@gmail.com>
2025-03-26 17:29:03 +0530

    updated all

--- src/sections/InternJobTitle.tsx
+++ src/sections/InternJobTitle.tsx
@@ -5,7 +5,7 @@ export default function InternJobTitle() {
             <h2 className="section-title mb-6">Internship Openings</h2>
             <p className="section-description mb-8">
               Our internship gives you real-world experience in software development,
-              <br /> where you'll work on live projects, solve complex problems, 
+              <br /> where you&apos;ll work on live projects, solve complex problems, 
               and learn from industry experts.
             </p>
           </div>
