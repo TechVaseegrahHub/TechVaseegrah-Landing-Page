0b789866da6c696e79ddb02434452455f51bf954
Prem <prmk1406@gmail.com>
2025-03-28 16:31:40 +0530

    updating...

--- src/components/constant.ts
+++ src/components/constant.ts
@@ -1,11 +1,11 @@
 export const items = [
-    "/assets/images/intern-1.jpeg",
-    "/assets/images/intern-2.jpeg",
-    "/assets/images/intern-3.jpeg",
-    "/assets/images/intern-4.jpeg",
-    "/assets/images/intern-1.jpeg",
-    "/assets/images/intern-3.jpeg",
-    "/assets/images/intern-4.jpeg",
-    "/assets/images/intern-1.jpeg",
+    "/assets/images/6.jpg",
+    "/assets/images/2.jpg",
+    "/assets/images/3.jpg",
+    "/assets/images/4.jpg",
+    "/assets/images/5.jpg",
+    "/assets/images/9.jpg",
+    "/assets/images/7.jpg",
+    "/assets/images/10.jpg",
   ];
   
